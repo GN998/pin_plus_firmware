bf27f6e19073f7f6d35445d844dce81cc23d96bc
Lucas <175045482+GN998@users.noreply.github.com>
2026-03-06 01:17:11 +0800

    Fix memory reclamation
    Fix memory reclamation

--- src/ch/token2/fido2/FIDO2Applet.java
+++ src/ch/token2/fido2/FIDO2Applet.java
@@ -1074,6 +1074,12 @@ public final class FIDO2Applet extends Applet implements ExtendedLength {
 
                 ok = true;
             } catch (Exception e) {
+                if (JCSystem.isObjectDeletionSupported()) {
+                    try {
+                    JCSystem.requestObjectDeletion();
+                } catch (Exception ignore) {
+                }
+            }
                 sendErrorByte(apdu, FIDOConstants.CTAP2_ERR_KEY_STORE_FULL);
             } finally {
                 if (ok) {
@@ -5040,6 +5046,11 @@ public final class FIDO2Applet extends Applet implements ExtendedLength {
         }
 
         // If we got here, we might not have found a matching credential. That's okay.
+        if ((numResidentCredentials & 0x03) == 0) {
+            if (JCSystem.isObjectDeletionSupported()) {
+                JCSystem.requestObjectDeletion();
+                }
+        }
         outBuf[0] = FIDOConstants.CTAP2_OK;
         sendNoCopy(apdu, (short) 1);
     }
@@ -5526,6 +5537,9 @@ public final class FIDO2Applet extends Applet implements ExtendedLength {
         } finally {
             if (ok) {
                 JCSystem.commitTransaction();
+                if (JCSystem.isObjectDeletionSupported()) {
+                    JCSystem.requestObjectDeletion();
+                }
                 sendErrorByte(apdu, FIDOConstants.CTAP2_OK);
             } else {
                 JCSystem.abortTransaction();
